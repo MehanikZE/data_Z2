1e0afcfee3a11ff55c148a3d220085150a64fcd3
Админ <admin@1>
2023-06-29 23:53:27 +0300

    release

--- main.py
+++ main.py
@@ -57,7 +57,6 @@ for filename in glob.glob('D:/Users/Админ/Downloads/egrul/*.json'):
             # print('Запись ОКВЭД не найдена')
 
 print("Отправлен список в бд")
-
 # print("Подсчет выполнен")
 # #
 # class Base(DeclarativeBase):
